5cee43f6a2c9275095825c3e785ccad164daa42e
L33t Hax0r <kja5wk@virginia.edu>
2015-05-03 19:54:30 -0400

    javascript

---
+++ transaction.js
@@ -0,0 +1,12 @@
+function transaction(num){
+    $.ajax({
+	url: "http://blockexporer.bitcoin-class.org/rawtx/" + num,
+	datatype: 'jsonp',
+	success: function(data){
+	    if( value.(*btcjson.TxRawResult).Confirmations > 5){
+		//execute pizza order
+		//but actually just send a new transaction to another address
+	    }
+	}
+    });
+}
